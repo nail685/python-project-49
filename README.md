48ca3e6149ecd2fb63a04e5ec0471a39260da9a8
Nail Girfanov <nail.ildarovich@gmail.com>
2024-08-01 20:42:33 +0500

    add README.md & brain_even

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/nail685/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/nail685/python-project-49/actions)
+
+download: git clone https://github.com/nail685/python-project-49.git\
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import prompt
+import random
+
+
+def game():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    i = 0
+    score = 0
+    while i < 3:
+        num = random.randint(1, 100)
+        print(f'Answer "yes" if the number is even, otherwise answer "no".'
+              f'\nQuestion: {num}')
+        answer = prompt.string('Your answer: ')
+        if num % 2 == 0 and answer == 'yes':
+            score += 1
+            print(f'Correct!')
+        elif num % 2 != 0 and answer == 'no':
+            score += 1
+            print(f'Correct!')
+        else:
+            right_answer = 'no' if num % 2 != 0 else 'yes'
+            print(f"{answer} is wrong answer ;(. Correct answer was"
+                  f" {right_answer}.Let's try again, {name}!")
+            break
+        i += 1
+    if score == 3:
+        print(f'Congratulations, {name}!')
+
+
+def main():
+    game()
+
+
+if __name__ == '__main__':
+    main()
